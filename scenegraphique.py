09d870754ae2569d9fd17f44a23cd9b5adabc7ca
loublanchard2 <151726793+loublanchard2@users.noreply.github.com>
2024-01-17 16:04:24 +0100

    Update scenegraphique.py

--- scenegraphique.py
+++ scenegraphique.py
@@ -7,14 +7,15 @@ class MaSceneGraphique(QGraphicsScene):
         super(MaSceneGraphique, self).__init__(parent)
 
         # Définir la taille de l'image (carrée)
-        image_size = 800
-        #l'image est retorunée à cause de l'inversion de l'axe y
+        image_size = 800   #l'image est retournée à cause de l'inversion de l'axe y
         image_path = 'grilleflipped.jpg' #chemin d'acces de l'image   
         pixmap = QPixmap(image_path)
         scaled_pixmap = pixmap.scaled(image_size, image_size, Qt.KeepAspectRatio)
+        
         # Calculer les coordonnées pour centrer l'image
         x_center = -image_size / 2
         y_center = -image_size / 2
+        
         # Ajouter l'image à la scène
         grid_item = QGraphicsPixmapItem(scaled_pixmap)
         grid_item.setPos(x_center, y_center) # permet de bien positionner l'image
