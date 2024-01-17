7f4088473689e7af83e91072aea9cf3cf65b8e75
jadecarpenter <151727018+jadecarpenter@users.noreply.github.com>
2024-01-17 15:43:12 +0100

    Update maindrone.py

--- maindrone.py
+++ maindrone.py
@@ -9,18 +9,11 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     print("lancement du programme")
     scene = scenegraphique.MaSceneGraphique()
-    
-    # Crée la vue graphique directement
-    #view = QGraphicsView(scene)
-    #view.show()
-
 
     voliere = drone_monitoring.ClientVoliere()
     fenetre = fenetres.MaFenetrePrincipale()
 
     voliere.drone_data.connect(fenetre.update_drone_data)
-    #if voliere.drone_data[0]==888:
-    #    tojason.Drone.posit=(voliere.drone_data[1],voliere.drone_data[2],voliere.drone_data[3])
 
     app.aboutToQuit.connect(voliere.stop)
 
